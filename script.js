862b9bd94ca15014667712d611b26ef2c1bbe4d6
Devrajshrma <devrajsharma0804@gmail.com>
2024-06-28 15:02:18 +0530

    hogya

---
+++ script.js
@@ -0,0 +1,30 @@
+tippy('#linkedin', {
+    content: 'Linkedin',
+    placement: 'top',
+    arrow: true,
+    animation: 'fade',
+    duration:500 ,
+    delay:50
+  });
+
+tippy('#Github', {
+    content: 'Github',
+    placement: 'top',
+    arrow: true,
+    animation: 'fade',
+    duration:500 ,
+    delay:50
+  });
+  
+tippy('#Resume', {
+    content: 'Resume',
+    placement: 'top',
+    arrow: true,
+    animation: 'fade',
+    duration:500 ,
+    delay:50
+  });
+
+  window.addEventListener("load", function(){
+    document.querySelector(".ldr").style.display='none'
+  })
