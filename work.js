1db5c41ae70cd6bb62ccb01a1ba6f29053d65640
Devrajshrma <devrajsharma0804@gmail.com>
2024-09-16 13:19:25 +0530

    kya kre

--- work.js
+++ work.js
@@ -1,7 +1,7 @@
 var projects = [
-    {value : "PROJECT 2", name :"Netflix Clone", img :"./img/2.png", a:"https://devrajshrma.github.io/nxui/", button:"<button>HTML</button><button>CSS</button><button>MediaQuery</button>"},
-    {value : "PROJECT 1", name :"Great Jones Clone", img :"./img/1.png", a:"https://devgreatjones.netlify.app/", button:"<button>HTML</button><button>CSS</button><button>MediaQuery</button>"},
-    {value : "PROJECT 3", name :"Amazone Clone", img :"./img/3.png", a:"https://devzon.netlify.app/", button:"<button>HTML</button><button>CSS</button><button>MediaQuery</button><button>JS</button>"},
+    {value : "PROJECT 1", name :"Netflix Clone", img :"./img/2.png", a:"https://devrajshrma.github.io/nxui/", button:"<button>HTML</button><button>CSS</button><button>MediaQuery</button>"},
+    {value : "PROJECT 2", name :"Great Jones Clone", img :"./img/1.png", a:"https://devgreatjones.netlify.app/", button:"<button>HTML</button><button>CSS</button><button>MediaQuery</button>"},
+    {value : "PROJECT 3", name :"Amazone Clone", img :"./img/3.png", a:"https://amvion-ecommerce.netlify.app/", button:"<button>HTML</button><button>CSS</button><button>MediaQuery</button><button>JS</button>"},
     {value : "PROJECT 4", name :"Pinterest Clone", img :"./img/4.png", a:"https://devtrest.netlify.app/", button:"<button>HTML</button><button>Tailwind</button><button>JS</button>"},
     {value : "PROJECT 5", name :"Fitness Freak Collaboration", img :"./img/last.png", a:"https://devfitnessfreak.netlify.app/", button:"<button>HTML</button><button>CSS</button><button>Bootstrap</button>"}
 ]
